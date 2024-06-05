bff741c27e9c4c7ff432db085b75eb6c72e209bc
MateuszBrudnik <98213264+MateuszBrudnik@users.noreply.github.com>
2024-06-05 15:23:11 +0200

    .

--- Angular/src/app/components/expenses/expenses.component.ts
+++ Angular/src/app/components/expenses/expenses.component.ts
@@ -1,8 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { MatTableDataSource } from '@angular/material/table';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { MatDialog } from '@angular/material/dialog';
 import { ExpenseService } from '../../services/expense.service';
 import { Expense } from '../../models/expense';
+import { AddExpenseDialogComponent } from './add-expense-dialog/add-expense-dialog.component';
 
 @Component({
   selector: 'app-expenses',
@@ -13,7 +15,11 @@ export class ExpensesComponent implements OnInit {
   displayedColumns: string[] = ['name', 'amount', 'date', 'category', 'actions'];
   dataSource = new MatTableDataSource<Expense>();
 
-  constructor(private expenseService: ExpenseService, private snackBar: MatSnackBar) { }
+  constructor(
+    private expenseService: ExpenseService,
+    private snackBar: MatSnackBar,
+    private dialog: MatDialog
+  ) { }
 
   ngOnInit(): void {
     this.loadExpenses();
@@ -29,6 +35,19 @@ export class ExpensesComponent implements OnInit {
     this.expenseService.deleteExpense(id).subscribe(() => {
       this.loadExpenses();
       this.snackBar.open('Expense deleted', 'Close', { duration: 2000 });
+    });
+  }
+
+  openAddExpenseDialog(): void {
+    const dialogRef = this.dialog.open(AddExpenseDialogComponent, {
+      width: '400px'
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      if (result) {
+        this.loadExpenses();
+        this.snackBar.open('Expense added', 'Close', { duration: 2000 });
+      }
     });
   }
 }
